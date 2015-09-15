c7c193f83b4ab1153c858218e2283edf10d9d207
blokeley <blokeley@gmail.com>
2015-09-15 17:45:14 +0100

    Fix #2 files downloaded multiple times if different members have different paths

--- tests/test_dfb.py
+++ tests/test_dfb.py
@@ -27,43 +27,11 @@ class TestGetMembers(unittest.TestCase):
         for i,  member_id in enumerate(dfb.get_members(None, response=resp)):
             self.assertEqual(expected[i], member_id)
 
+class TestRemoveUnprintable(unittest.TestCase):
 
-class TestPath(unittest.TestCase):
-
-    def setUp(self):
-        self.unshared = dfb.Path(12, r'/folder/file.txt')
-        self.lower = dfb.Path(123,
-                              r'/unshared/shared folder/subfolder/file.txt',
-                              r'/shared folder')
-        self.mixed = dfb.Path(456,
-                              r'/Unshared/Shared Folder/Subfolder/FILE.txt',
-                              r'/Shared Folder')
-        self.repeated = dfb.Path(789,
-                                 r'/Unshared/Shared Folder/Subfolder/'
-                                 r'Shared Folder/Subfolder2/FILE.txt',
-                                 r'/Shared Folder')
-
-    def test_no_shared(self):
-        expected = r'/folder/file.txt'
-        self.assertEqual(expected, self.unshared.shared_path)
-
-    def test_lower(self):
-        expected = r'/shared folder/subfolder/file.txt'
-        self.assertEqual(expected, self.lower.shared_path)
-
-    def test_mixed(self):
-        expected = r'/Shared Folder/Subfolder/FILE.txt'
-        self.assertEqual(expected, self.mixed.shared_path)
-
-    def test_repeat(self):
-        expected = (r'/Shared Folder/Subfolder/Shared Folder/Subfolder2/'
-                    'FILE.txt')
-        self.assertEqual(expected, self.repeated.shared_path)
-
-    def test_equal(self):
-        mixed = dfb.Path(543, r'/Shared Folder/Subfolder/FILE.txt')
-        self.assertTrue(mixed == self.mixed)
-        self.assertFalse(self.lower == self.mixed)
+    def test_remove(self):
+        input = 'some\u200c text'
+        self.assertEqual('some text', dfb.remove_unprintable(input))
 
 
 class TestDateFormat(unittest.TestCase):
